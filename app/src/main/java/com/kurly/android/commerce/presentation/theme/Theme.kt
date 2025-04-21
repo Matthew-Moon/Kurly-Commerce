d54821a367813f8624f8d4dee10dcbef77f88648
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-04-21 23:24:37 +0900

    feat: ui -> presentation 으로 디렉토리 이름 변경, 할인율 색상코드 추가

--- app/src/main/java/com/kurly/android/commerce/presentation/theme/Theme.kt
+++ app/src/main/java/com/kurly/android/commerce/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.kurly.android.commerce.ui.theme
+package com.kurly.android.commerce.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
