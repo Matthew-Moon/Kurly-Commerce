3fe5e2971cbbed21d2bed82b103e8a06e6a94669
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-04-23 23:30:33 +0900

    chore: 불 필요한 파일 제거 및 컬러리소스 추가

--- app/src/main/java/com/kurly/android/commerce/ui/theme/Color.kt
+++ app/src/main/java/com/kurly/android/commerce/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.kurly.android.commerce.presentation.theme
+package com.kurly.android.commerce.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,4 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val DiscountRateColor = Color(0xFFFA622F)
+
+val ProductName = Color(0xFF656565)
+val ProductPrice = Color(0xFF262626)
+val DiscountLate = Color(0xFFFA622F)
+
+val CancelPrice = Color(0xFFB0AFAF)
+
+val KurlyColor = Color(0xFF5f0080)
